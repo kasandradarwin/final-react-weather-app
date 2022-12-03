c0211edd4a3225a02b8089345388b1168a2b8d15
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-12-03 09:54:17 -0800

    added github url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Kasandra D and is {" "}
+      
+      <a href="https://github.com/kasandradarwin/final-react-weather-app" target="_blank">
+         open-sourced on Github
+      </a>
+    
+      
+    </footer>
     </div>
   );
 }
