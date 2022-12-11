b800045b38834388167a38d636636b3edca18ffd
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-12-10 21:34:54 -0800

    fixed forecast date

--- src/components/weather/WeatherForecastDay.js
+++ src/components/weather/WeatherForecastDay.js
@@ -1,21 +1,28 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
 
 export default function WeatherForecastDay(props){
 
     function day() {
-        let date = new Date(props.data.time)
+        let date = new Date(props.data.time * 1000)
         let day = date.getDay();
-
+        
         let days = ["Sun","Mon","Tues","Wed","Thurs","Fri","Sat",]
+        console.log("DATE",date)
+        console.log("DAY",day)
+        console.log("+++++++++++",date)
 
-        console.log(props)
+        // console.log(props)
         return days[day]
     }
+
     return(  
 <>
     <div className="WeatherForecast-day"><strong>{day()}</strong></div>
-        <WeatherIcon code={props.data.condition.icon} size={36} />
+    {/* { console.log("***************************",props.data.time) } */}
+        <WeatherIcon code={props.data.condition.icon} size={100} />
+        {/* {console.log()} */}
         <div className="weatherForecast-temperature-max"><small> {props.data.condition.description} </small></div>
         <div className="WeatherForecast-temperatures" >
             <span className="weatherForecast-temperature-max"><small>{Math.round(props.data.temperature.maximum)}° <span className="divider">| </span>  </small></span>
